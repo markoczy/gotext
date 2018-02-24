46b2902072a33bf246cbec94f4e50b1c41375634
Markoczy Alain Alistair <alainalistair.markoczy@students.bfh.ch>
2018-02-24 11:47:54 +0100

    more commands

--- handler/handler.go
+++ handler/handler.go
@@ -1,29 +1,32 @@
 package handler
 
-import 
-(
-	"log"
+import (
+	"regexp"
 	"strings"
+
 	"github.com/markoczy/goutil/cli"
 	"github.com/markoczy/goutil/cli/parser"
+	"github.com/markoczy/goutil/log"
 )
 
 var cmdParser = initParser()
 
-const OK_NO_VALUE = "OK_NO_VALUE"
+var xNewLine = regexp.MustCompile("\r?\n")
 
-func Exec(args []string, input string) (*string,error) {
-	log.Printf("Command array: %v\n",args)
-	log.Printf("Input: %s\n",input)
-	
-	if len(args)<1 {
+const StrOkNoValue = "OK_NO_VALUE"
+
+func Exec(args []string, input string) (*string, error) {
+	log.Debugf("Command array: %v\n", args)
+	log.Debugf("Input: %s\n", input)
+
+	if len(args) < 1 {
 		showHelp()
 		// not set clipboard
 		return nil, nil
 	}
 
-	lArgs := append(args,input)
-	ifc, err := parser.Exec(&cmdParser, lArgs)
+	lArgs := append(args, input)
+	ifc, err := cmdParser.Exec(lArgs)
 	ret := ifc.(string)
 	return &ret, err
 }
@@ -32,14 +35,70 @@ func showHelp() {
 
 }
 
-func initParser() parser.Parser {
-	parser := cli.NewParser();
+// func AddCommand(aParser parser.Parser, aName string, aPriority int,
+// 	aRegex string, aArgCount int, aOperation command.Operation) error {
 
-	cli.AddCommand(parser,"Uppercase","u",1,uppercase)
-	
-	return *parser
+func initParser() parser.Parser {
+	parser := cli.NewParser()
+
+	cli.AddCommand(parser, "Uppercase", 1, "u", 1, uppercase)
+	cli.AddCommand(parser, "Lowercase", 1, "l", 1, lowercase)
+	cli.AddCommand(parser, "Prefix", 1, "p", 2, prefix)
+	cli.AddCommand(parser, "Suffix", 1, "s", 2, suffix)
+	cli.AddCommand(parser, "Trim start", 1, "ts", 2, trimStart)
+	cli.AddCommand(parser, "Trim start (exclusive)", 2, "tsx", 1, trimStartX)
+	cli.AddCommand(parser, "Trim end", 1, "te", 2, trimEnd)
+	cli.AddCommand(parser, "Trim end (exclusive)", 2, "tex", 1, trimEndX)
+
+	return parser
 }
 
 func uppercase(s []string) (interface{}, error) {
 	return strings.ToUpper(s[1]), nil
 }
+
+func lowercase(s []string) (interface{}, error) {
+	return strings.ToLower(s[1]), nil
+}
+
+func prefix(s []string) (interface{}, error) {
+
+	// split := strings.Split(s[2], "\n")
+	var ret string
+	split := xNewLine.Split(s[2], -1)
+	for _, e := range split {
+		log.Debug("Element: " + e)
+		ret += s[1] + e + "\n"
+	}
+
+	return ret, nil
+}
+
+func suffix(s []string) (interface{}, error) {
+	return strings.ToLower(s[1]), nil
+}
+
+func trimStart(s []string) (interface{}, error) {
+	return strings.ToLower(s[1]), nil
+}
+
+func trimStartX(s []string) (interface{}, error) {
+	return strings.ToLower(s[1]), nil
+}
+
+func trimEnd(s []string) (interface{}, error) {
+	return strings.ToLower(s[1]), nil
+}
+
+func trimEndX(s []string) (interface{}, error) {
+	return strings.ToLower(s[1]), nil
+}
+
+// todo: replace, filter, merge, ...
+
+// func isValidCount(s []string, req int) {
+// 	if req == -1 {
+// 		return true
+// 	}
+// 	return len(s) == req
+// }
--- main.go
+++ main.go
@@ -3,28 +3,28 @@ package main
 import
 (
 	"os"
-	"log"
 	"github.com/atotto/clipboard"
+	"github.com/markoczy/goutil/log"
 	"github.com/markoczy/gotext/handler"
 )
 
 func main() {
 	var clip, err = clipboard.ReadAll();
 	if err != nil {
-		log.Panicln("Something went wrong: "+err.Error())
+		log.Error("Something went wrong: "+err.Error())
 		return
 	}
 	
 	s, err := handler.Exec(os.Args[1:], clip)
 	if err != nil {
-		log.Panicln("Something went wrong: "+err.Error())
+		log.Error("Something went wrong: "+err.Error())
 		return
 	}
 
 	if s != nil {
-		log.Println("New Value found, writing")
+		log.Debug("New Value found, writing")
 		clipboard.WriteAll(*s) 
 	}
 
-	log.Println("Nomal Exit")
+	log.Debug("Nomal Exit")
 }
